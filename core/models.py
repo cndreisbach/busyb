4e01a5e3a4c1620f8be463a8b388683849d71ab4
Clinton Dreisbach <clinton@dreisbach.us>
2019-01-15 16:41:23 -0500

    Add task filters

--- core/models.py
+++ core/models.py
@@ -1,7 +1,9 @@
 from django.db import models
+from django.db.models import Q
 
 from django.contrib.auth.models import AbstractUser
 from core.hashids import hashids
+from datetime import date
 
 
 class User(AbstractUser):
@@ -16,6 +18,22 @@ class TaskQuerySet(models.QuerySet):
             return self.get(pk=ids[0])
         return self.filter(pk__in=ids)
 
+    def incomplete(self):
+        return self.filter(complete=False)
+
+    def complete(self):
+        return self.filter(complete=True)
+
+    def visible(self):
+        return self.filter(
+            Q(show_on__isnull=True) | Q(show_on__lte=date.today()))
+
+    def todos_for_user(self, user):
+        """
+        This is the standard set a user should see: for them, visible, incomplete.
+        """
+        return self.filter(owner=user).incomplete().visible()
+
 
 class Task(models.Model):
 
@@ -26,6 +44,9 @@ class Task(models.Model):
     description = models.CharField(max_length=512)
     owner = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name='tasks')
+    complete = models.BooleanField(default=False)
+    due_on = models.DateField(null=True, blank=True)
+    show_on = models.DateField(null=True, blank=True)
 
     @property
     def hashid(self):
--- core/tests.py
+++ core/tests.py
@@ -1,3 +1,49 @@
-from django.test import TestCase
+import pytest
+from datetime import date, timedelta
+from core.models import Task, User
 
-# Create your tests here.
+
+@pytest.mark.django_db
+@pytest.fixture
+def user():
+    return User.objects.create_user('test', 'test@example.org')
+
+
+@pytest.mark.django_db
+def test_task_visibility(user):
+    task_no_show_on = Task.objects.create(description='task0', owner=user)
+    task_show_on_today = Task.objects.create(
+        description='task1', show_on=date.today(), owner=user)
+    task_show_on_tomorrow = Task.objects.create(
+        description='task2',
+        show_on=date.today() + timedelta(days=1),
+        owner=user)
+
+    visible_tasks = Task.objects.visible()
+    assert task_no_show_on in visible_tasks
+    assert task_show_on_today in visible_tasks
+    assert task_show_on_tomorrow not in visible_tasks
+
+
+@pytest.mark.django_db
+def test_todos_for_user(user):
+    other_user = User.objects.create_user('test2')
+
+    task_no_show_on = Task.objects.create(description='task0', owner=user)
+    task_show_on_today = Task.objects.create(
+        description='task1', show_on=date.today(), owner=user)
+    task_complete = Task.objects.create(
+        description='complete', owner=user, complete=True)
+    task_show_on_tomorrow = Task.objects.create(
+        description='task2',
+        show_on=date.today() + timedelta(days=1),
+        owner=user)
+    task_other_user = Task.objects.create(
+        description='other_user_task', owner=other_user)
+
+    todos = Task.objects.todos_for_user(user)
+    assert task_no_show_on in todos
+    assert task_show_on_today in todos
+    assert task_show_on_tomorrow not in todos
+    assert task_other_user not in todos
+    assert task_complete not in todos
