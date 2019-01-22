efbabbb4fe946eb7064c22e128219271a336dc40
Clinton Dreisbach <clinton@dreisbach.us>
2019-01-22 14:22:41 -0500

    Add django-heroku

--- busyb/settings.py
+++ busyb/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -151,3 +152,6 @@ STATICFILES_DIRS = [
 ]
 
 INTERNAL_IPS = ['127.0.0.1']
+
+# Configure Django App for Heroku.
+django_heroku.settings(locals())
