8a214338a70202b41264a917efeed9559f7306e3
Clinton Dreisbach <clinton@dreisbach.us>
2019-04-04 10:53:10 -0400

    Allow new tasks to be created via the API

--- api/urls.py
+++ api/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from api import views
 
 urlpatterns = [
-    path('tasks/', views.task_list),
+    path('tasks/', views.TaskList.as_view()),
     path('tasks/<int:pk>/', views.task_detail),
 ]
--- api/views.py
+++ api/views.py
@@ -1,9 +1,20 @@
+import json
+
+from django.contrib.auth.models import AnonymousUser
 from django.http import JsonResponse
+from django.utils.decorators import method_decorator
+from django.views.decorators.csrf import csrf_exempt
+from django.views.generic.base import View
 
-from core.models import User
+from core.models import Task, User
 
 
-def get_user(request):
+def check_user(request):
+    if not request.user.is_authenticated:
+        request.user = get_user_from_token(request)
+
+
+def get_user_from_token(request):
     authorization_header = request.META['HTTP_AUTHORIZATION']
     if authorization_header.startswith("Token"):
         token = authorization_header[6:]
@@ -11,33 +22,64 @@ def get_user(request):
             user = User.objects.get(api_token=token)
             return user
         except User.DoesNotExist:
-            return None
-    return None
+            return AnonymousUser()
+    return AnonymousUser()
 
 
-# Create your views here.
-def task_list(request):
-    user = get_user(request)
-    if not user:
+@method_decorator(csrf_exempt, name='dispatch')
+class TaskList(View):
+
+    def get(self, request):
+        check_user(request)
+        if not request.user.is_authenticated:
+            return JsonResponse({
+                "status": "error",
+                "message": "unauthorized",
+            },
+                                status=401)
+
+        tasks = request.user.tasks.all()
+        tasks_data = [task.to_dict() for task in tasks]
+        return JsonResponse({"status": "ok", "data": tasks_data})
+
+    def post(self, request):
+        check_user(request)
+        if not request.user.is_authenticated:
+            return JsonResponse({
+                "status": "error",
+                "message": "unauthorized",
+            },
+                                status=401)
+
+        task_data = json.loads(request.body)
+
+        if task_data.get('description'):
+            task = Task(
+                owner=request.user, description=task_data['description'])
+            task.save()
+            return JsonResponse({
+                "status": "ok",
+                "data": task.to_dict()
+            },
+                                status=201)
+
         return JsonResponse({
             "status": "error",
-            "message": "unauthorized",
+            "errors": {
+                "description": "cannot be blank"
+            }
         },
-                            status=401)
-
-    tasks = user.tasks.all()
-    tasks_data = [task.to_dict() for task in tasks]
-    return JsonResponse({"status": "ok", "data": tasks_data})
+                            status=400)
 
 
 def task_detail(request, pk):
-    user = get_user(request)
-    if not user:
+    check_user(request)
+    if not request.user.is_authenticated:
         return JsonResponse({
             "status": "error",
             "message": "unauthorized",
         },
                             status=401)
 
-    task = user.tasks.get(pk=pk)
+    task = request.user.tasks.get(pk=pk)
     return JsonResponse({"status": "ok", "data": task.to_dict()})
